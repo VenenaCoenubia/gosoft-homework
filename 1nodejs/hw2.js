aa6ad46b13bd2d8d13aac8b06c38320868fbdef2
Chinnawat Uthaisawak <champ3599@gmail.com>
2022-05-08 13:33:02 +0700

    งานBADCครั้งที่2

--- 1nodejs/hw2.js
+++ 1nodejs/hw2.js
@@ -1,9 +1,15 @@
 const a_z = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 let output = "";
-
+async function Coenubia(){
+    try{
 for(let i=0; i<26; i+=2){
     output += a_z[i+1] + " " + a_z[i] + " ";
 }
 
-console.log("OUTPUT:",output)
+await console.log("OUTPUT:",output);
+}catch{
+console.error("error");
+}
+}
+Coenubia();
